474726a1da40bef1250232562bedc2eaab62f945
jefersonfernandes539 <jeferson@almosthackers.io>
2025-07-03 12:08:29 -0300

    feat: creation migration

--- Program.cs
+++ Program.cs
@@ -31,6 +31,11 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>();
+    dbContext.Database.Migrate();
+}
 
 
 if (app.Environment.IsDevelopment())
